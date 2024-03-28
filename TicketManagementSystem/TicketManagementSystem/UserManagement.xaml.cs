da6a33bdb86d3018264acc821a046afc53319868
Johnny622 <johnny_0622@hotmail.com>
2024-03-28 16:32:33 +0800

    update Johnny

---
+++ TicketManagementSystem/TicketManagementSystem/UserManagement.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TicketManagementSystem
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class UserManagement : Page
+    {
+        public UserManagement()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnBar_Click(object sender, RoutedEventArgs e)
+        {
+            if (menuSplitView.IsPaneOpen == false) { menuSplitView.IsPaneOpen = true; rightContent.Margin = new Thickness(270, 0, 0, 0); }
+            else if (menuSplitView.IsPaneOpen == true) { menuSplitView.IsPaneOpen = false; rightContent.Margin = new Thickness(80, 0, 0, 0); }
+        }
+
+        private void btnUserMng_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
